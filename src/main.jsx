ad6a7810a60eb9f568b430ac779c9c03bc9d91d1
sumit kumar <sumitku3500@gmail.com>
2024-08-31 19:38:35 +0530

    j

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-// import "./index.css";
-import './Index.css';
-// import Contact from "./components/contact/Contact.jsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    
   </StrictMode>
 );
