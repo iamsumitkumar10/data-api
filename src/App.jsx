13f5c2c8e8d267b3873202061f1cda5db299c3b2
sumit kumar <sumitku3500@gmail.com>
2024-09-01 16:17:46 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header/Header.jsx";
-import About from "./components/About/About.jsx";
+import About from "./components/about/About.jsx";
 import Contact from "./components/contact/Contact.jsx";
 import Home from "./components/Home/Home.jsx";
 import Footer from "./components/Footer/Footer.jsx";
