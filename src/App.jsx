36697a42915186c4f5aa06bad4aeb36d4a97e8a3
sumit kumar <sumitku3500@gmail.com>
2024-09-01 16:26:00 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/header/Header.jsx";
 import About from "./components/about/About.jsx";
 import Contact from "./components/contact/Contact.jsx";
 import Home from "./components/Home/Home.jsx";
-import Footer from "./components/Footer/Footer.jsx";
+import Footer from "./components/footer/Footer.jsx";
 import Hireme from "./components/hireme/Hireme.jsx";
 
 function App() {
